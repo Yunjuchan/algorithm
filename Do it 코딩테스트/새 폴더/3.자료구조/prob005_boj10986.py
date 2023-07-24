2c5119607ff14eee1661e4647d78a3c2f0bf51b1
juchan <wncks2546@gmail.com>
2023-07-24 16:56:18 +0900

    나머지배열 난이도 상

--- Do it 코딩테스트/새 폴더/3.자료구조/prob005_boj10986.py
+++ Do it 코딩테스트/새 폴더/3.자료구조/prob005_boj10986.py
@@ -1,11 +1,22 @@
-N = int(input())
-total = 0
-i = 1
-while N > total :
-    total += i
-    i += 1
-cnt = total - N
-if i % 2 == 1 :
-    print(f'{i-1-cnt}/{1+cnt}')
-else :
-    print(f'{1+cnt}/{i-1-cnt}')
+# 나머지합 구하기
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+sum_arr = [0]
+cnt_arr = [0] * M
+tmp = 0
+for i in arr :
+    tmp += i 
+    sum_arr.append(tmp)
+
+# res_arr와 cnt_arr를 한번의 for문에 입력받았음
+res_arr = list(map(lambda x : x % M, sum_arr))
+
+for res in res_arr :
+    cnt_arr[res] += 1
+result = cnt_arr[0]
+
+for cnt in cnt_arr :
+    if cnt > 1 :
+        result += cnt * (cnt-1) // 2
+
+print(result)
