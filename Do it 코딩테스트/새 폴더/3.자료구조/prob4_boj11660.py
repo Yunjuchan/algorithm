67df607e201143cc062ce36a9c4d47294a613757
윤주찬 <wncks2546@gmail.com>
2023-07-25 21:00:27 +0900

    약수의합 풀이

---
+++ Do it 코딩테스트/새 폴더/3.자료구조/prob4_boj11660.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+for m in range(M) :
+    arr = []
+    for i in range(N) :
+        arr.appned(list(map(int, input().split())))
+    x1, y1, x2, y2 = map(int, input().split())
--- code plus/0005_BOJ17425.py
+++ code plus/0005_BOJ17425.py
@@ -1,3 +1,4 @@
+## 17427을 이용하려고 했지만 확장이 되지 않았음.
 # import sys
 # input = sys.stdin.readline
 # T = int(input())
@@ -8,8 +9,26 @@
 #         total += (N // i) * i
 #     print(total)
 
-T = 9
-total = [0] * (T+1)
-for i in range(1, T+1) :
-    total[i] = (T // i) * i + total[i-1]
-print(total)
+
+# 약수의 개념으로 들어가서 약수일때만 호출될 수 있도록 출력하여 nlogn으로
+
+import sys
+input = sys.stdin.readline
+n = 1000000
+div_arr = [1] * (n + 1)
+accu_arr = [0] * (n + 1)
+accu_arr[1] = 1
+for i in range(2, n+1) :    # 약수 : C
+    for j in range(1, (n // i) + 1) :   # 배수 : B
+        div_arr[i * j] += i     # i * j 가 원래 수 : A
+    accu_arr[i] = accu_arr[i-1] + div_arr[i]
+T = int(input())
+for t in range(T) :
+    num = int(input())
+    print(accu_arr[num])
+
+## python3은 컴파일 속도가 매우 느림 -> pypy3가 컴파일 속도가 빨라서
+## 대체하였더니 잘 나왔음
+
+## 약수문제에서 힘들었던 부분 : A에서 B와 C를 찾아내려고 했던게 시간복잡도를 향상시켰음
+## 곱셈이나 두 개를 합쳐서 큰 값을 만들어낼때 불필요한 호출수 를 줄일 수 있음
