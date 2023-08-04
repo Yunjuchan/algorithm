4c2b02b11e5f60bf738b65686c59b682d80aacb7
윤주찬 <wncks2546@gmail.com>
2023-08-04 15:41:23 +0900

    신기한 소수 찾기

---
+++ Do it 코딩테스트/5.탐색/prob024_2023.py
@@ -0,0 +1,41 @@
+# 숫자는 최대 1억
+import sys
+N = int(input())
+# 내 풀이 메모리 초과
+# prime = [True] * (10 ** 8 + 1)
+# prime[0] = prime[1] = False
+# for i in range(2, 10 ** 4 + 1) :
+#     for j in range(2, (10 ** 4 + 1) // i) :
+#         prime[i*j] = False
+
+# def DFS(x) :
+#     if x == 0 :
+#         return 1
+#     if prime[x] :
+#         if not DFS(x // 10) :
+#             return 0
+#     else : return 0
+    
+#     return 1
+
+def is_prime(x) :
+    for i in range(2, int(x**(1/2))+1) :
+        if x % i == 0 :
+           return 0
+        else :
+            continue
+    return 1
+
+def DFS(x) :
+    if len(str(x)) == N and is_prime(x):
+        print(x)
+    else :
+        if is_prime(x) : 
+            for i in range(1, 10, 2) :
+                DFS(x*10 + i)
+
+
+DFS(2)
+DFS(3)
+DFS(5)
+DFS(7)
--- Do it 코딩테스트/새 폴더/test.py
+++ Do it 코딩테스트/새 폴더/test.py
@@ -1,20 +1,34 @@
-num = list(map(int, input()))
-bucket = [0] * 10
+import sys
+input = sys.stdin.readline
+N = int(input())
+switch = [0] + list(map(int, input().split()))
+def boy(x) :
+    for i in range(1, N//x+1) :
+        switch[x*i] = (switch[x*i] + 1) % 2
 
-set_buck = bucket[0]
-cnt_69 = 0
+def girl(x) :
+    i = 1
+    while True :
+        i += 1
+        if x-i == 0 or x+i == N :
+            i -= 1
+            break
+        if switch[x-i] == switch[x+i] :
+            continue
+        else :
+            x -= 1
+            break
+    while i != 0 :
+        switch[x-i] = (switch[x-i] + 1) % 2
+        switch[x+i] = (switch[x+i] + 1) % 2
+        i -= 1
+    switch[x] = (switch[x] + 1) % 2
 
-for i in range(len(num)):
-    if num[i] == 6 or num[i] == 9 :
-        cnt_69 += 1
+M = int(input())
+for i in range(M) :
+    s, n = map(int, input().split())
+    if s == 1 :
+        boy(n)
     else :
-        bucket[num[i]] += 1
-for i in range(len(bucket)):
-    if bucket[i] > set_buck:
-        set_buck = bucket[i]
-
-set_69 = (cnt_69 + 1) // 2
-
-if set_69 >= set_buck:
-    print(set_69)
-else: print(set_buck)
+        girl(n)
+print(*switch[1:])
