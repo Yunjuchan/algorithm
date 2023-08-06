72f15ac81f9037476d42db68cae56081de8fa092
윤주찬 <wncks2546@gmail.com>
2023-08-06 18:33:17 +0900

    세준수

--- Do it 코딩테스트/새 폴더/test.py
+++ Do it 코딩테스트/새 폴더/test.py
@@ -4,18 +4,19 @@ N = int(input())
 K = int(input())
 prime = [True] * (N+1)
 prime[0] = prime[1] = False
-cnt = 0
-soinsu = [1]
+sejun = [1] * (N+1)
+sejun[0] = 0
 for i in range(2, int(N ** (1/2)) + 1) :
     for j in range(i, (N // i) + 1) :
         prime[i*j] = False
 
-for i in range(1, N+1) :
-    for p, bl in enumerate(prime[:]) :
-        if bl and i % p == 0 :
-             soinsu.append(p)
-    if soinsu[-1] <= K :
-        cnt += 1
+for i in range(2, N+1) :
+    if prime[i] and i > K :
+        for j in range(1, N // i + 1) :
+            sejun[i*j] = 0
+cnt = 0
+for i in range(N+1) :
+    cnt += sejun[i]
 print(cnt)
 
 
