bee1a9ff0f281c791329f0d1919b4f3d3c081d6b
윤주찬 <wncks2546@gmail.com>
2023-08-07 01:13:02 +0900

    에디터일반풀이

---
+++ Do it 코딩테스트/새 폴더/에디터 풀이 미완성.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+word = input().strip()
+cursor = len(word)
+N = int(input())
+
+for i in range(N) :
+    x = input().strip().split()
+    if x[0] == 'L' and cursor > 0 :
+        cursor -= 1
+    elif x[0] == 'D' and cursor < len(word) :
+        cursor += 1
+    elif x[0] == 'B' and cursor > 0 :
+        word = word[:cursor-1] + word[cursor:]
+        cursor -= 1
+    elif x[0] == 'P' :
+        word = word[:cursor] + x[1] + word[cursor:]
+        cursor += 1
+    else :
+        pass
+print(word)
