ba4b7364102577768296efcae75a880666c3f90d
윤주찬 <wncks2546@gmail.com>
2023-09-05 00:40:12 +0900

    알고문제풀이

--- 2.py
+++ 2.py
@@ -1,20 +1,2 @@
-N, M = map(int, input().split())
-dic = dict()
-cnt = 0
-lst = []
-for _ in range(N) :
-    dic[input()] = 1
-
-for _ in range(M) :
-    x = input()
-    dic[x] = dic.get(x,0) + 1
-    if dic[x] == 2 :
-        cnt += 1
-    
-print(cnt)
-for k, v in dic.items() :
-    if v == 2 :
-        lst.append(k)
-lst.sort()
-for i in lst :
-    print(i)
+lst = [1,2,3,4,5]
+print(lst[1:-1])
