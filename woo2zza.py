7304d6c2e6eb37923a70a8ae6e6f6b936f500882
윤주찬 <wncks2546@gmail.com>
2023-09-08 00:39:41 +0900

    stra

--- woo2zza.py
+++ woo2zza.py
@@ -1,30 +1,40 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-def dfs(x) :
-    for i in adj[x]     :
-        if not visited[i] :
-            visited[i] = True
-            dfs(i)
-N, M, k = map(int, input().split())
-lst = list(enumerate(map(int, input().split())))
-adj = [set([i]) for i in range(N+1)]
-visited = [False]*(N+1)
-visited[0] = True
-lst.sort(key=lambda x : x[1])
-for _ in range(M) :
-    s, e = map(int, input().split())
-    adj[s].add(e)
-    adj[e].add(s)
-total = 0
-for i, p in lst :
-    if k-p < 0 : break
-    if not visited[i+1] :
-        k -= p
-        total += p
-        dfs(i+1)
-if sum(visited) == N+1 :
-    print(total)
-else :
-    print('Oh no')
+T = int(input())
+for _ in range(T) :
+
+    K, M, P = map(int, input().split())
+    adj = [[] for _ in range(M+1)]
+    lst = [0]*(M+1)
+    stra = [[0,0] for _ in range(M+1)]
+    for _ in range(P) :
+        s, e = map(int, input().split())
+        adj[s].append(e)
+        lst[e] += 1
+    que = deque()
     
+    for i in range(1, M+1) :
+        if lst[i] == 0 :
+            stra[i] = [1,1]
+            que.append([i, 1])
+    
+    while que :
+        now, level = que.popleft()
+        
+        for i in adj[now] :
+            lst[i] -= 1
+            if stra[i][0] < level :
+                stra[i] = [level,1]
+            elif stra[i][0] == level :
+                stra[i][1] += 1
+            
+            if lst[i] == 0 :
+                if stra[i][1] == 1 :
+                    que.append([i, stra[i][0]])
+                else :
+                    que.append([i, stra[i][0]+1])
+    result = stra[M][0]
+    if stra[M][1] != 1 :
+        result += 1
+    print(K, result)
