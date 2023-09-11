20268c95c199417b7c2e6debcb6766e581e015f8
윤주찬 <wncks2546@gmail.com>
2023-09-11 22:46:42 +0900

    일타싸피

--- woo2zza.py
+++ woo2zza.py
@@ -1,42 +1,15 @@
-from collections import deque
-import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
-N, M = map(int, input().split())
-arr = [input() for _ in range(N)]
+A, B = map(int, input().split())
 
-def bfs(y, x, level) :
-    global cnt
-    que = deque()
-    que.append([y, x])
-    while que :
-        c_y, c_x = que.popleft()  
-        visited[c_y][c_x] = level
-        n_y, n_x = command(c_y, c_x, arr[c_y][c_x])
-        if visited[n_y][n_x] != -1 :
-            if visited[n_y][n_x] == level :
-                cnt += 1
-            return
-        else :
-            que.append([n_y, n_x])
-        
-def command(y, x, c) :
-    if c == 'D' :
-        y += 1
-    elif c == 'U' :
-        y -= 1
-    elif c == 'R' :
-        x += 1
-    else :
-        x -= 1
-    return y, x
-
-visited = [[-1]*M for _ in range(N)]
-
-cnt = level = 0
-for i in range(N) :
-    for j in range(M) :
-        if visited[i][j] == -1:
-            bfs(i, j, level)
-            level += 1
-print(cnt)
+def count(num) :
+    i = 0
+    lst = [0] * 60
+    while num :
+        tmp = num // 2**i
+        lst[i] = ((tmp + 1) // 2) * 2 ** i
+        if lst[i] == 0 :
+            break
+        if tmp % 2 != 0 :
+            lst[i] -= (tmp+1)*2**(i) - 1 - num
+        i += 1
+    return sum(lst)
+print(count(B) - count(A-1))
