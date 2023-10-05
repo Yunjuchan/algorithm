32dea7592aed4cb210024c728fcbb2ca24fc80dd
윤주찬 <wncks2546@gmail.com>
2023-10-05 17:31:09 +0900

    선긋기

---
+++ 프로그래머스/정렬/가장큰수.py
@@ -0,0 +1,14 @@
+numbers = [3, 30, 34, 5, 9]
+def solution(numbers):
+    numbers.sort(reverse=True)
+    numbers = list(map(str, numbers))
+    print(numbers)
+    for num in numbers :
+        i = num[0]
+        if not num_list[i] :
+            num_list[i].append(num)
+            continue
+        for x in range()
+    answer = ''    
+    return answer
+print(solution(numbers))
