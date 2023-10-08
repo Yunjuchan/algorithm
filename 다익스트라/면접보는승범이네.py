5ffc578df50d8534305b0d43598d96f48940bc9e
윤주찬 <wncks2546@gmail.com>
2023-10-08 21:24:30 +0900

    1008

--- 다익스트라/면접보는승범이네.py
+++ 다익스트라/면접보는승범이네.py
@@ -3,39 +3,33 @@ import sys
 input = sys.stdin.readline
 N, M, K = map(int, input().split())
 adj = [[] for _ in range(N+1)]
-INF = 21e8
+INF = 21e13
 
 for _ in range(M) :
     s, e, d = map(int, input().split())
     adj[e].append([s, d])
 
-L = list(map(int, input().split())) 
+R = list(map(int, input().split()))
 
-def dijk(start) :
-    que = []
-    dist = [INF] * (N+1)
-    dist[start] = 0
-    visited = [False] * (N+1)
-    heappush(que, [0, start])
-    while que :
-        d, now = heappop(que)
-        if visited[now] : continue
-        visited[now] = True
-        for next, next_d in adj[now] :
-            if dist[next] > d+next_d :
-                dist[next] = d+next_d
-                heappush(que, [dist[next], next])
-    for i in range(1, N+1) :
-        result_dist[i] = min(dist[i], result_dist[i])
-    return 
+que = []
+dist = [INF] * (N+1)
+visited = [False] * (N+1)
+for i in R :
+    heappush(que,[0, i])
+    dist[i] = 0
 
-result_dist = [INF] * (N+1)
-for i in L :
-    dijk(i)
-Max = 0
+while que :
+    d, now = heappop(que)
+    if visited[now] : continue
+    visited[now] = True
+    for next, next_d in adj[now] :
+        if dist[next] > d+next_d :
+            dist[next] = d+next_d
+            heappush(que, [dist[next], next])
+Max = -1
 for i in range(1, N+1) :
-    if result_dist[i] > Max :
-        Max = result_dist[i]
+    if dist[i] > Max :
+        Max = dist[i]
         Max_i = i
 print(Max_i)
 print(Max)
