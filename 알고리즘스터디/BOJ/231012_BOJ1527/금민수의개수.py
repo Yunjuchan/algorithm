4f2cca476133069a1769284fb6735e0ead16b0b7
윤주찬 <wncks2546@gmail.com>
2023-10-12 22:11:11 +0900

    금민수

---
+++ 알고리즘스터디/BOJ/231012_BOJ1527/금민수의개수.py
@@ -0,0 +1,15 @@
+A, B = map(int, input().split())
+L = ['4', '7']
+cnt = 0
+def dfs(x) :
+    global cnt
+    if A <= int(x) <= B :
+        cnt += 1
+    if int(x) > B :
+        return
+    for i in L :
+        dfs(x+i)
+    
+dfs('0')
+
+print(cnt)
