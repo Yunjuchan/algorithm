98143f95747e641f79425b57baf0c00154527f24
윤주찬 <wncks2546@gmail.com>
2023-10-24 00:02:01 +0900

    알고리즘 풀어다랄느거 풀어줌

--- woo2zza.py
+++ woo2zza.py
@@ -1,26 +1,48 @@
 N = int(input())
-stack = [[int(input()), 1]]
-n = 1   # 스택의 길이
-result = 0
-for _ in range(N-1) :
-    x = int(input())
-    while stack :
-        if stack[-1][0] < x :
-            result += stack[-1][1]
-            stack.pop()
-            n -= 1
-        elif stack[-1][0] == x : 
-            result += stack[-1][1]
-            stack[-1][1] += 1
-            if n != 1 :
-                result += 1
-            break
+dice = [input() for _ in range(N)]
+visited = [False] * N
+path = [-1]*N
+value = [-1]*N
+Min = 21e8
+Max = -21e8
+def is_cycle(i, j, level) :
+    if i == path[j] :
+        return level
+    return is_cycle(i, path[j], level+1)
+        
+def dfs(level, path, value) :
+    global Min, Max
+    if level == N :
+        mul = 1
+        cycle = [0]*N
+        for i in range(N) :
+            tmp = 0
+            cycle[is_cycle(i, i, 0)] += 1
+        for i in range(N) :
+            tmp += cycle[i] // (i+1)
+        if  not tmp % 2 :
+            mul *= -1
+        for x in value :
+            mul *= x
+        if Min > mul :
+            Min = mul
+        if Max < mul :
+            Max = mul
+        return
+    
+    for i in range(N) :
+        if visited[i] == True : continue
+        visited[i] = True
+        path[level] = i
+        if dice[level][i].isdigit() :
+            value[level] = int(dice[level][i])
         else :
-            result += 1
-            stack.append([x,1])
-            n += 1
-            break
-    else :
-        stack.append([x, 1])
-        n += 1
-print(result)
+            value[level] = -(ord(dice[level][i])-64)
+        dfs(level+1, path, value)
+        value[level] = 0
+        path[level] = -1
+        visited[i] = False
+
+dfs(0, path, value)
+print(Min)
+print(Max)
--- 다익스트라/다리만들기.py
+++ 다익스트라/다리만들기.py
@@ -1,61 +1,57 @@
-from heapq import heappop, heappush
 from collections import deque
 import sys
 input = sys.stdin.readline
 N = int(input().rstrip())
 island = [list(map(int, input().split())) for _ in range(N)]
-island2 = [[21e8]*N for _ in range(N)]
+# island2 = [[21e8]*N for _ in range(N)]
+island2 = [[9]*N for _ in range(N)]
 
 def check(idx, y, x) :
     que = deque()
     que.append((y, x))
     while que :
+        flag = 1
         y, x = que.popleft()
         island2[y][x] = 0
-        island[y][x] = -idx
         for i in range(4) :
             dy = direct_y[i] + y
             dx = direct_x[i] + x
             if dy < 0 or dx < 0 or dy >= N or dx >= N : continue
-            if island[dy][dx] == 0 :
-                heappush(pq, (0, -idx, y, x))
-            elif island[dy][dx] == 1 :
-                island[dy][dx] = 0
+            if island[dy][dx] == 1 :
                 que.append((dy, dx))
-
+                island[dy][dx] = -idx
+            elif island[dy][dx] == 0 :
+                flag = 0
+        if flag == 0 :
+            que2.append((-idx, y, x))
+                
 
 direct_y = [1,0,-1,0]
 direct_x = [0,1,0,-1]
-pq = []
+que2 = deque()
 cnt = 1
 for i in range(N) :
     for j in range(N) :
         if island[i][j] != 1 : continue
-        island[i][j] = 0
+        island[i][j] = -cnt
         check(cnt, i, j)
         cnt += 1
 
-for a in island :
-    print(*a)
-
-while pq :
-    ret = 21e8
-    cnt, tp, y, x = heappop(pq)
-    print(tp)
+ret = 21e8
+while que2 :
+    tp, y, x = que2.popleft()
     for i in range(4) :
         dy = direct_y[i] + y
         dx = direct_x[i] + x
         if dy < 0 or dx < 0 or dy >= N or dx >= N : continue
-        if island2[dy][dx] == 21e8  :
-            island2[dy][dx] = cnt + 1
-            heappush(pq, (cnt+1, dy, dx))
+
+        if island2[dy][dx] == 9 :
+            island[dy][dx] = tp
+            island2[dy][dx] = island2[y][x] + 1
+            que2.append((tp, dy, dx)) 
         else :
-            if ret > island2[dy][dx] + cnt :
-                ret = island2[dy][dx] + cnt
-    if ret != 21e8 :
-        break
-print()
-for a in island2 :
-    print(*a)
+            if island[dy][dx] == tp : continue
+            if ret > island2[dy][dx] + island2[y][x] :
+                ret = island2[dy][dx] + island2[y][x]
+
 print(ret)
-            
