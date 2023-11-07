ae82d16d14bd8dd9bcbd17e4db165f8d7a6a25f8
윤주찬 <wncks2546@gmail.com>
2023-11-07 11:58:10 +0900

    욕심쟁이 판다

--- DP/판다.py
+++ DP/판다.py
@@ -1,27 +1,33 @@
 direct_y = [1,0,-1,0]
 direct_x = [0,1,0,-1]
-def dfs(y, x, level) :
-    global Max 
-    if level > Max :
-        Max = level
+
+def dfs(y, x) :
+    if dp[y][x] != 1 :
+        return dp[y][x]
     for i in range(4) :
         dy = direct_y[i] + y
         dx = direct_x[i] + x
         if dy < 0 or dy >= N or dx < 0 or dx >= N : continue
-        if arr[dy][dx] < arr[y][x] :
-            visited[dy][dx] = True
-            dfs(dy, dx, level+1)
+
+        if arr[dy][dx] > arr[y][x] :
+            ret = dfs(dy, dx)
+            dp[y][x] = max(dp[y][x], ret+1)
+
+    return dp[y][x]
+        
 import sys
 input = sys.stdin.readline
-N = int(input())
+sys.setrecursionlimit(10**7)
+
+N = int(input().rstrip())
 arr = [list(map(int, input().split())) for _ in range(N)]
 dp = [[1]*N for _ in range(N)]
-visited = [[False] * N for _ in range(N)]
 
 Max = 1
 for i in range(N) :
     for j in range(N) :
-        if visited[i][j] : continue
-        visited[i][j] = True
-        dfs(i, j, 1)
+        if dp[i][j] != 1 : continue
+        dfs(i, j)
+        if Max < dp[i][j] :
+            Max = dp[i][j]
 print(Max)
